0dd52f4307d182a6237c55fcfaf7a3b02092cd89
Jihyo Jung <jihuo1004@gmail.com>
2022-06-13 16:42:36 +0900

    first commit

---
+++ src/main/java/com/ez/launer/LaunerApplication.java
@@ -0,0 +1,13 @@
+package com.ez.launer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LaunerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LaunerApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/ez/launer/configuration/MvcConfiguration.java
@@ -0,0 +1,36 @@
+package com.ez.launer.configuration;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.multipart.commons.CommonsMultipartResolver;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class MvcConfiguration implements WebMvcConfigurer{
+
+	@Override
+	public void addInterceptors(InterceptorRegistry registry) {
+//		// 레지스트리에 인터셉터 등록
+//		registry.addInterceptor(new LoginInterceptor())
+//		.addPathPatterns("/shop/cart/*", "/shop/order/*", 
+//				"/member/memberEdit", "/member/memberOut");
+//		//=> 작성한 특성 url에만 LoginInterceptor를 적용하겠다는 뜻
+//		
+//		registry.addInterceptor(new AdminLoginInterceptor())
+//		.excludePathPatterns("/admin/login/adminLogin")	
+//		//=> 인터셉처 설정한 경로중에서, 제외할 경로 설정(예외처리)
+//		.addPathPatterns("/admin/**");	
+////=> 같은뜻! => .addPathPatterns("/admin/*/*", "/admin/*");	
+	}
+
+	@Bean
+	public CommonsMultipartResolver multipartResolver() {
+		CommonsMultipartResolver multipartResolver 
+			= new CommonsMultipartResolver();
+		multipartResolver.setDefaultEncoding("UTF-8"); // 파일 인코딩 설정
+		multipartResolver.setMaxUploadSizePerFile(2 * 1024 * 1024); // 파일당 업로드 크기 제한 (2MB)
+		return multipartResolver;
+	}
+	
+}
