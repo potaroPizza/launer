cb6f5b9820b817efdbd8e9387048c9c556d543d8
HAN-SEOHYUN <105329813+HAN-SEOHYUN@users.noreply.github.com>
2022-06-16 23:49:32 +0900

    Merge branch 'main' of https://github.com/potaroPizza/launer.git into seohyun

--- src/main/java/com/ez/launer/laundryService/order/controller/orderController.java
+++ src/main/java/com/ez/launer/laundryService/order/controller/orderController.java
@@ -1,10 +1,19 @@
 package com.ez.launer.laundryService.order.controller;
 
+import java.util.Arrays;
+
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Map;
+
+import javax.servlet.http.HttpServletRequest;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 
 
@@ -31,6 +40,32 @@ public class orderController {
 		
 		return "/laundryService/order/orderConfirm";
 	}
+	
+	
+	
+	
+	   @ResponseBody
+	    @RequestMapping("/paramPost")
+	    public Map<String, String> rrrr(HttpServletRequest request) {
+	        logger.info("post넘김");
+	        String[] itemArr = request.getParameterValues("categoryNo");
+
+	        HashMap<String, String> mapmap = new HashMap<String, String>();   //아이템이랑 개수가 담길 변수? 맞나
+	        HashSet<String> setset = new HashSet<>(Arrays.asList(itemArr));   //이걸로 중복 제거해줄 거임
+
+	        String[] resultItemArr = setset.toArray(new String[0]);     //중복제거 완료?
+
+	        for (String value : resultItemArr) {    //중복없는 값
+	            int itemChk = 0;
+	            for (String s : itemArr) {
+	                if (s.equals(value)) itemChk++;
+	            }
+
+	            mapmap.put(value, String.valueOf(itemChk));
+	        }
+
+	        return mapmap;
+	    }
 	
 	
 }
