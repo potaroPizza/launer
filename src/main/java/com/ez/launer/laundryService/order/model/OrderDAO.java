3b5a0c1ae5b525e12cacb69935b6c3fe2810f4f8
HAN-SEOHYUN <105329813+HAN-SEOHYUN@users.noreply.github.com>
2022-06-22 19:32:32 +0900

    order.xml 에러 해결 OK

--- src/main/java/com/ez/launer/laundryService/order/model/OrderDAO.java
+++ src/main/java/com/ez/launer/laundryService/order/model/OrderDAO.java
@@ -13,8 +13,9 @@ public interface OrderDAO {
     List<OrderDeliveryAllVO> orderOfficeView(OrderListSearchVO orderListSearchVO);
 
     int orderCount(OrderListSearchVO orderListSearchVO);
-    Map<String, Object> selectUsersOrderView(int usersNo);
+    OrderViewVO selectUsersOrderView(int usersNo);
     int insertOrder(OrderVO orderVo);
+    
     
     
     
