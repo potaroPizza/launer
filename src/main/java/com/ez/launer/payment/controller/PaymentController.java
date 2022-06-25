d81331658bbde1a8cc7c7323b11ae13560e0c9ee
HAN-SEOHYUN <105329813+HAN-SEOHYUN@users.noreply.github.com>
2022-06-25 22:42:28 +0900

    결제실패 시 사용된 포인트 되돌리기

--- src/main/java/com/ez/launer/payment/controller/PaymentController.java
+++ src/main/java/com/ez/launer/payment/controller/PaymentController.java
@@ -1,5 +1,7 @@
 package com.ez.launer.payment.controller;
 
+import javax.servlet.http.HttpSession;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
@@ -38,32 +40,45 @@ public class PaymentController {
 	public String requestPayment_post(Model model, int orderNo,int payPrice,int userPoint, RedirectAttributes reAttr) {
 		logger.info("fk주문번호={}",orderNo);
 		logger.info("결제금액={}",payPrice);
+		logger.info("userPoint={}",userPoint);
 		
 		PaymentVO paymentVo = new PaymentVO();
 		paymentVo.setOrderNo(orderNo);
 		paymentVo.setPayPrice(payPrice);
 		
 		int result = paymentService.insertPaymentDetail(paymentVo);
+		logger.info("결제성공여부 result={}",result);
 		
 		//orders 테이블 paymentDate null => sysdate
-		
-		int rs =0;
-		if(result>0) {
-			rs = orderService.updatePaymentDate(orderNo);
-		}else if(result<=0) {
-			
-			//결제 실패 시 point 원상복구
-			UserVO userVo = userService.selectById(orderNo);
-			userVo.setPoint(userPoint);
-			rs = orderService.updateUserPoint(userVo);
-		}
-
-		logger.info("결제성공여부 result={}",result);
+		int rs = orderService.updatePaymentDate(orderNo);
 		logger.info("결제일(date)update rs={}",rs);
+
 		
 		reAttr.addAttribute("paymentCode", paymentVo.getPaymentCode());
 		reAttr.addAttribute("paymentStatus", rs);
 		
 		return "/launer/laundryService/payment/orderPayment";
 	}
+	
+	//결제 실패 시
+	@GetMapping("/paymentFailed")
+	public String paymentFailed_get(Model model,int orderNo, int payPrice, int userPoint, HttpSession session) {
+		int no =1000;
+		//(String) session.getAttribute("userid");
+
+		logger.info("결제실패, orderNo={}",orderNo);
+		logger.info("결제실패, payPrice={}",payPrice);
+		logger.info("결제실패, userPoint={}",userPoint);
+		
+		UserVO userVo = userService.selectById(no);
+		logger.info("userVo={}",userVo);
+		
+		userVo.setPoint(userPoint);
+		int rs = orderService.updateUserPoint(userVo);
+		logger.info("결제취소, 업데이트 포인트 결과={}",rs);
+		return "/launer";
+	}
+	
+	
+	
 }
