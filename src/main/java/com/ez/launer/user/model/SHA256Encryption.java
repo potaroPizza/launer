807b8fec8bb0eb4764e6c11a9325663ec9905244
HAN-SEOHYUN <105329813+HAN-SEOHYUN@users.noreply.github.com>
2022-07-03 02:50:18 +0900

    회원가입시 비밀번호 암호화 OK

---
+++ src/main/java/com/ez/launer/user/model/SHA256Encryption.java
@@ -0,0 +1,33 @@
+package com.ez.launer.user.model;
+
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class SHA256Encryption {
+	public String encrypt(String pwd) throws NoSuchAlgorithmException  {
+		try {
+			MessageDigest md = MessageDigest.getInstance("SHA-256");
+			md.update(pwd.getBytes());
+			byte byteData[] = md.digest();
+			StringBuffer sb = new StringBuffer();
+			for (int i = 0; i < byteData.length; i++) {
+				sb.append(Integer.toString((byteData[i] & 0xff) + 0x100, 16).substring(1));
+			}
+			StringBuffer hexString = new StringBuffer();
+			for (int i = 0; i < byteData.length; i++) {
+				String hex = Integer.toHexString(0xff & byteData[i]);
+				if (hex.length() == 1) {
+					hexString.append('0');
+				}
+				hexString.append(hex);
+			}
+			return hexString.toString();
+		} catch (Exception e) {
+			e.printStackTrace();
+			throw new RuntimeException();
+		}
+	}
+}
