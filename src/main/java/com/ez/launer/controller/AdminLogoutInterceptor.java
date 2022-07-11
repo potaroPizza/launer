d638c6f595cd1f1ac4354190c7ff64825d59426a
potaroPizza <105497269+potaroPizza@users.noreply.github.com>
2022-07-11 19:18:35 +0900

    관리자 인터셉터 구현2

--- src/main/java/com/ez/launer/controller/AdminLogoutInterceptor.java
+++ src/main/java/com/ez/launer/controller/AdminLogoutInterceptor.java
@@ -1,6 +1,5 @@
 package com.ez.launer.controller;
 
-
 import java.io.PrintWriter;
 
 import javax.servlet.http.HttpServletRequest;
@@ -9,35 +8,41 @@ import javax.servlet.http.HttpSession;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.ui.Model;
 import org.springframework.web.servlet.HandlerInterceptor;
 
-public class AdminLoginInterceptor implements HandlerInterceptor {
-	private static final Logger logger = LoggerFactory.getLogger(AdminLoginInterceptor.class);
+public class AdminLogoutInterceptor implements HandlerInterceptor {
+	private static final Logger logger = LoggerFactory.getLogger(AdminLogoutInterceptor.class);
 
 	@Override
 	public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler)
 			throws Exception {
-		logger.info("관리자 로그인체크 인터셉터 실행");
+		logger.info("관리자 로그아웃체크 인터셉터 실행");
 
 		HttpSession session = request.getSession();
 
 		String adminEmail = (String) request.getSession().getAttribute("adminEmail");
 		logger.info("adminEmail = {}", adminEmail);
 
-		if(adminEmail == null || adminEmail.isEmpty()) {
+		if(adminEmail != null) {
 			response.setContentType("text/html; charset = UTF-8");
 			PrintWriter out = response.getWriter();
 			out.print("<script type='text/javascript'>");
-			out.print("alert('관리자 페이지를 이용하시려면"
-					+ "\\n먼저 로그인 해주세요');");
-			out.print("location.href = '" + request.getContextPath() + "/admin/adminLogin';");
+			out.print("alert('관리자 페이지를 벗어나 보안을 위해,"
+					+ "\\n자동적으로 관리자 계정은 로그아웃됩니다.');");
+			out.print("location.href = '" + request.getContextPath() + "/';");
 			out.print("</script>");
+			
+//			session.removeAttribute("adminEmail");
+			session.invalidate();
+			session = request.getSession();
+			
+			response.setHeader("cache-control","no-cache");
+			response.setHeader("expires","0");
+			response.setHeader("pragma","no-cache");
 
 			return false;
 		}
 
-		session.setAttribute("CHK_LOGIN", true);
-		return true;	//다음 컨트롤러가 수행됨
+		return true;
 	}
 }
