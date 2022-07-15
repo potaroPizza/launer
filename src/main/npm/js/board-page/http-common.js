386dd245ef6c86dd8101f1126f29e9525eb60430
Davi9506 <105350268+Davi9506@users.noreply.github.com>
2022-07-15 17:50:12 +0900

    3조화이팅

--- src/main/npm/js/board-page/http-common.js
+++ src/main/npm/js/board-page/http-common.js
@@ -1,11 +1,11 @@
 import axios from "axios";
 export default axios.create({
-    //baseURL : "http://localhost:9095/launer/api",
-    baseURL : "http://192.168.0.118:9095/launer/api",	// 지효 학원컴 주소
+    baseURL : "http://localhost:9095/launer/api",
+    //baseURL : "http://192.168.0.22:9095/launer/api",	// 지효 학원컴 주소
     headers:{
         "Content-type":"application/json",
-        //'Access-Control-Allow-Origin': 'http://localhost:9095' // 서버 domain
-         'Access-Control-Allow-Origin': 'http://192.168.0.118:9095'	// 지효 학원컴 도메인
+        'Access-Control-Allow-Origin': 'http://localhost:9095' // 서버 domain
+         //'Access-Control-Allow-Origin': 'http://192.168.0.22:9095'	// 지효 학원컴 도메인
     },
     withCredentials: true
 });
