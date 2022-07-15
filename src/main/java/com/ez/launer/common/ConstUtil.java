fc02887d14a2e1a1459fbe12677116bb772fe02b
honey6352 <105294300+honey6352@users.noreply.github.com>
2022-07-15 23:19:00 +0900

    Merge branch 'main' of https://github.com/potaroPizza/launer.git into sung

--- src/main/java/com/ez/launer/common/ConstUtil.java
+++ src/main/java/com/ez/launer/common/ConstUtil.java
@@ -12,13 +12,12 @@ public interface ConstUtil {
 
 	//String FILE_UPLOAD_TYPE = "deploy";	// 배포시
 
-//	String FILE_UPLOAD_TYPE = "deploy";	// 배포시
 
 	
 	//자료실 - 파일 저장 경로
 //	String TEST_FILE_PATH = "C:\\lecture\\workspace_list\\sung_ws\\launer\\src\\main\\resources\\static\\upload\\";
-	String TEST_FILE_PATH = "/Library/IntelliJ Folder/project/src/main/resources/static/upload/";	//for Mac
-//	String TEST_FILE_PATH = "C:\\lecture\\workspace_list\\sung_ws\\launer\\src\\main\\resources\\static\\upload\\";	//for Window
+//	String TEST_FILE_PATH = "/Library/IntelliJ Folder/project/src/main/resources/static/upload/";	//for Mac
+	String TEST_FILE_PATH = "C:\\lecture\\workspace_list\\sung_ws\\launer\\src\\main\\resources\\static\\upload\\";	//for Window
 	String FILE_UPLOAD_PATH = "pd_upload";
 //	String FILE_UPLOAD_PATH_TEST = "C:\\lecture\\workspace_list\\spboot_ws\\springherb\\src\\main\\resources\\static\\pds_upload";
 //	String FILE_UPLOAD_PATH_TEST = TEST_FILE_PATH + FILE_UPLOAD_PATH;	//Mac
