df6665a84216777b19c1dc0e8f0805234dd2eb27
potaroPizza <105497269+potaroPizza@users.noreply.github.com>
2022-07-21 09:56:36 +0900

    hi

--- src/main/java/com/ez/launer/common/ConstUtil.java
+++ src/main/java/com/ez/launer/common/ConstUtil.java
@@ -16,9 +16,10 @@ public interface ConstUtil {
 
 	
 	//자료실 - 파일 저장 경로
+//	String TEST_FILE_PATH = "C:\\lecture\\workspace_list\\git_ws\\launer\\src\\main\\resources\\static\\upload\\";	//정인 집
 //	String TEST_FILE_PATH = "C:\\lecture\\workspace_list\\sung_ws\\launer\\src\\main\\resources\\static\\upload\\";
-	String TEST_FILE_PATH = "/Library/IntelliJ Folder/project/src/main/resources/static/upload/";	//for Mac
-//	String TEST_FILE_PATH = "C:\\lecture\\workspace_list\\sung_ws\\launer\\src\\main\\resources\\static\\upload\\";	//for Window
+//	String TEST_FILE_PATH = "/Library/IntelliJ Folder/project/src/main/resources/static/upload/";	//for Mac
+	String TEST_FILE_PATH = "C:\\lecture\\workspace_list\\git_ws\\launer\\src\\main\\resources\\static\\upload\\";	//for Window
 	String FILE_UPLOAD_PATH = "pd_upload";
 //	String FILE_UPLOAD_PATH_TEST = "C:\\lecture\\workspace_list\\spboot_ws\\springherb\\src\\main\\resources\\static\\pds_upload";
 //	String FILE_UPLOAD_PATH_TEST = TEST_FILE_PATH + FILE_UPLOAD_PATH;	//Mac
